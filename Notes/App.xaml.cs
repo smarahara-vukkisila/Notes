6993550c042441e4354a8cf85cdd2dfa9d6cc046
Smarahara Vukkisila <smavuk@amazon.com>
2020-08-14 17:14:33 +0530

    Basic funcationality implementation

--- Notes/App.xaml.cs
+++ Notes/App.xaml.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Notes.Data;
 
 namespace Notes
 {
     public partial class App : Application
     {
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new NotesPage());
         }
 
         protected override void OnStart()
